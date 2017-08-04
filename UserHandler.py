51ecb4eae9a0766ce74422801dbfd2b1e94edf30
Emily etal <emily.mi.darrow@gmail.com>
2017-08-04 07:13:45 +0000

    freaking finally

--- UserHandler.py
+++ UserHandler.py
@@ -10,8 +10,11 @@ class UserHandle():
         self.email = email
         self.mariaCon = None
         self.cursor = None
+        self.uid = None
+        self.uniqueUser=None
 
         self.ConnectToDB()
+
 
     def ConnectToDB(self):
         #creating MySQLConnection object using connect() constructor to the MySQL server. 
@@ -23,7 +26,7 @@ class UserHandle():
 
     def DoesUserExist(self):
         '''Determines is user exists. Returns true if user exists,
-        returns false if user does not exist'''
+        returns false and number of users with same email in a tuple (email, num_of_dupliates)'''
         query = "SELECT email FROM User WHERE email = %s "
 
         self.cursor.execute(query,(self.email,) )
@@ -37,12 +40,14 @@ class UserHandle():
         if i == 0:
             return False
 
-        else:
-            return True
+        elif i > 0:
+            return (True, i)  #if 1 then just returns 1 which is also true? Works vecause only use negate
 
     def AddNewUser(self):
         '''If user sucessifully added, will return "success". If user already exists, 
         then returns "error" and user is not added'''
+        if not self.name or not self.pwd or not self.email:
+            return "Error: User already exists. Please try a different email address."
 
         add_user = ("INSERT INTO User "
                     " (name, password, email) "
@@ -57,40 +62,91 @@ class UserHandle():
 
             #commit to database
             self.mariaCon.commit()
+
+            #need to get userid and store
+            query_uid = "SELECT uid FROM User WHERE email = %s"
+            self.cursor.execute(query_uid, (self.email,))
+
+            self.uid = self.cursor.fetchone()[0]
+
             print("User: ", self.name, "added to database")
-            return "success"
+            return "sucess"
 
         #USER ALREADY EXISTS, DONT ADD
         else:
+            print("User: ", self.name, " already exists, not added!")
             return "Error: User already exists. Please try a different email address."
 
-    def PopulateUserPage(self, about, research):
-        #Add about PI to database
-        add_about = ("INSERT INTO Page "
-                    " (about) "
-                    "VALUES  (%s)")
+    def PopulatePage(self, about, research):
 
-        self.cursor.execute(add_about, (about,) )
+        #Add about and research to Page
 
-        #Add reserach to database
-        add_research = ("INSERT INTO Page "
-                    " (research) "
+        add_info = ("INSERT INTO Page (user, about, research) "
                     "VALUES  (%s, %s, %s)")
 
-        self.cursor.execute(add_about, (about,) )
-
-
-
-
+        self.cursor.execute(add_info, (self.uid, about, research))
+        self.mariaCon.commit()
 
         #once user page is populted, connection is closed
-        self.DisconnectDB
+        #self.DisconnectDB
 
+class RetrieveUserInfo():
+    def __init__(self, email):
+        self.email = email
+        self.uid = None
+
+        self.ConnectToDB()
+
+    def ConnectToDB(self):
+        #creating MySQLConnection object using connect() constructor to the MySQL server. 
+        self.mariaCon = mysql.connector.connect(user='flask', host ='localhost', password = 'flaskpass' , database ='scifinder')
+        self.cursor = self.mariaCon.cursor()
+
+    def GetUid(self):
+        query_uid = "SELECT uid FROM User WHERE email = %s"
+        self.cursor.execute(query_uid, (self.email,))
+
+        self.uid = self.cursor.fetchone()[0]
+        return self.uid
+
+
+    def CheckLogin(self,password):
+        query = "SELECT email FROM User WHERE email = %s AND password = %s "
+
+        self.cursor.execute(query,(self.email,password) )
+
+        if self.cursor.fetchone() is None:
+            return False
+        else:
+            return True
+
+
+    def DisconnectDB(self):
+        self.mariaCon.close()
+
+    def GetPage(self):
+        query_uid = "SELECT Page.research, Page.about FROM User join Page on User.uid=Page.user WHERE User.email = %s "
+        self.cursor.execute(query_uid, (self.email,))
+        return self.cursor.fetchone()
 
 
 
 if __name__ == "__main__":
-    new = UserHandle("99Bryce", "99whatever", "99hello@gmail.com")
+    #new = UserHandle("5Ronjo", "5new here", "5WORKING@gmail.com")
 
-    new.AddNewUser()
-    new.PopulateUserPage("My name is Bryce hello", "I like to research Frisbees")
+    #new.AddNewUser()
+    #new.PopulatePage("THIS WHOULD LKJLKJ", "BITEME!!!")
+
+    retr = RetrieveUserInfo("5WORKING@gmail.com")
+    print("UID: ",retr.GetUid())
+    print("Research and About ", retr.GetPage())
+    if retr.CheckLogin("5new here" ):
+        print("login worked")
+    else:
+        print("login failed")
+
+
+
+
+
+
