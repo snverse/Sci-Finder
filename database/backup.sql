44b1e82b2de5f8958ede83e713acd4912daef5bd
Emily etal <emily.mi.darrow@gmail.com>
2017-08-04 05:39:23 +0000

    forgot database on git

---
+++ database/backup.sql
@@ -0,0 +1,96 @@
+CREATE TABLE User (
+	name		VARCHAR(99),
+	password	CHAR(32),
+	email		VARCHAR(255),
+	location 	VARCHAR(255),
+	uLevel 		TINYINT UNSIGNED,
+	ORCID 		INT,
+	uid 		INT UNSIGNED PRIMARY KEY AUTO_INCREMENT
+);
+
+CREATE TABLE Tag (
+	topic 		INT UNSIGNED,
+	technique 	INT UNSIGNED,
+	uid 		INT UNSIGNED PRIMARY KEY AUTO_INCREMENT
+);
+
+CREATE TABLE Lab (
+	name		VARCHAR(255),
+	uid		INT UNSIGNED PRIMARY KEY AUTO_INCREMENT
+);
+
+CREATE TABLE Page (
+	research	MEDIUMBLOB,
+	about		MEDIUMBLOB,
+	image		MEDIUMBLOB,
+	uid		INT UNSIGNED PRIMARY KEY AUTO_INCREMENT
+);
+
+CREATE TABLE Dataset (
+	id_pot		VARCHAR(255),
+	link		VARCHAR(255),
+	uid		INT UNSIGNED PRIMARY KEY AUTO_INCREMENT
+);
+
+CREATE TABLE Pub (
+	doi		VARCHAR(255),
+	title		VARCHAR(255),
+	link		VARCHAR(255),
+	uid		INT UNSIGNED PRIMARY KEY AUTO_INCREMENT
+);
+
+CREATE TABLE User_Page (
+	user		INT UNSIGNED,
+	page		INT UNSIGNED,
+	FOREIGN KEY (user) REFERENCES User (uid),
+	FOREIGN KEY (page) REFERENCES Page (uid)
+);
+
+CREATE TABLE Tag_Width (
+	user		INT UNSIGNED,
+	tag		INT UNSIGNED,
+	FOREIGN KEY (user) REFERENCES User (uid),
+	FOREIGN KEY (tag) REFERENCES Tag (uid)
+);
+
+CREATE TABLE Lab_Team (
+    	lab         	INT UNSIGNED,
+    	user        	INT UNSIGNED,
+    FOREIGN KEY (lab) REFERENCES Lab (uid),
+    FOREIGN KEY (user) REFERENCES User (uid)
+);
+
+CREATE TABLE PI (
+	pub		INT UNSIGNED,
+	user		INT UNSIGNED,
+	FOREIGN KEY (pub) REFERENCES Pub (uid),
+	FOREIGN KEY (user) REFERENCES User (uid)
+);
+
+CREATE TABLE Uses_Technique (
+	pub		INT UNSIGNED,
+	technique	INT UNSIGNED,
+	FOREIGN KEY (pub) REFERENCES Pub (uid),
+	FOREIGN KEY (technique) REFERENCES Tag (uid)
+);
+
+CREATE TABLE Uses_Data (
+	pub		INT UNSIGNED,
+	dataset		INT UNSIGNED,
+	FOREIGN KEY (pub) REFERENCES Pub (uid),
+	FOREIGN KEY (dataset) REFERENCES Dataset (uid)
+);
+
+CREATE TABLE Pub_Author (
+	pub         	INT UNSIGNED,
+	user        	INT UNSIGNED,
+	FOREIGN KEY (pub) REFERENCES Pub (uid),
+	FOREIGN KEY (user) REFERENCES User (uid)
+);
+
+CREATE TABLE Produced_By (
+	pub         	INT UNSIGNED,
+	lab         	INT UNSIGNED,
+	FOREIGN KEY (pub) REFERENCES Pub (uid),
+	FOREIGN KEY (lab) REFERENCES Lab (uid)
+);
